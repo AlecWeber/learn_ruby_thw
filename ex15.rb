7786eb06f048bbd16dbce1de3dbb48a9df2dff22
Alec <aweber@arrayschool.com>
2017-01-03 16:59:07 -0700

    tuesday progress

--- ex15.rb
+++ ex15.rb
@@ -1,13 +1,9 @@
+# command prompt to store filename in var
 file_name = ARGV.first
-
+# store the open filename method in a var
 txt = open(file_name)
-
+# print "Here's your (whatever filename is)"
 puts "Here's your file #{file_name}:"
+# print the text in the file in the terminal
 print txt.read
-
-print "Type the file name again: "
-file_again = $stdin.gets.chomp
-
-txt_again = open(file_again)
-
-print txt_again.read
+txt.close
